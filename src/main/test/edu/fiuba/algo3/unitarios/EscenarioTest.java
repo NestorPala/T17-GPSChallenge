eb71f87ee078db4dadd8c7375b62c7253e1fe85c
MatiasTK <matiavallejo@gmail.com>
2022-06-06 19:01:40 -0300

    feat: aumentada la covertura de las pruebas

--- src/main/test/edu/fiuba/algo3/unitarios/EscenarioTest.java
+++ src/main/test/edu/fiuba/algo3/unitarios/EscenarioTest.java
@@ -1,16 +1,27 @@
 package edu.fiuba.algo3.unitarios;
 
-import edu.fiuba.algo3.modelo.ElementosDelEscenario.ElementoDelEscenario;
 import edu.fiuba.algo3.modelo.ElementosDelEscenario.Pozo;
 import edu.fiuba.algo3.modelo.ElementosDelEscenario.Vacio;
 import edu.fiuba.algo3.modelo.Escenario;
 import edu.fiuba.algo3.modelo.EscenarioConLimitesInvalidosError;
 import edu.fiuba.algo3.modelo.Posicion;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EscenarioTest {
+
+    @Test
+    public void siIntentoCrearUnEscenarioConLimitesInvalidosMeTiraError() {
+        Executable tarea1 = () -> new Escenario(-5,5);
+
+        assertThrows(EscenarioConLimitesInvalidosError.class, tarea1);
+
+        Executable tarea2 = () -> new Escenario(5,-5);
+
+        assertThrows(EscenarioConLimitesInvalidosError.class, tarea2);
+    }
 
     @Test
     public void chequeoUnaPosicionDelEscenarioInvalidaMeDaFalse() throws EscenarioConLimitesInvalidosError {
@@ -47,5 +58,19 @@ public class EscenarioTest {
         assertTrue(escenario.obtenerElementoEnEscenario(new Posicion(5,5)) instanceof Vacio);
         escenario.ponerUnElemento(pozo, new Posicion(5,5));
         assertTrue(escenario.obtenerElementoEnEscenario(new Posicion(5,5)) instanceof Pozo);
+    }
+
+    @Test
+    public void siIntentoObtenerUnElementoEnUnaPosicionFueraDeLosLimitesReciboNull() throws EscenarioConLimitesInvalidosError {
+        Escenario escenario = new Escenario(10,10);
+
+        assertNull(escenario.obtenerElementoEnEscenario(new Posicion(11,11)));
+    }
+
+    @Test
+    public void puedoSaberSiUnaPosicionEstaDentroDelEscenario() throws EscenarioConLimitesInvalidosError {
+        Escenario escenario = new Escenario(10,10);
+
+        assertFalse(escenario.estaDentroDelEscenario(new Posicion(11,11)));
     }
 }
