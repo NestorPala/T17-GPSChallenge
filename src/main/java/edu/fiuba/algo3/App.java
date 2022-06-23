a2889e21c103ca0a7cf76ae13c0ba9e6127e8136
lorenzoahumada <lorenzosahumada@gmail.com>
2022-06-23 17:28:37 -0300

    Pequeños cambios en App

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -2,6 +2,7 @@ package edu.fiuba.algo3;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -13,11 +14,20 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        StackPane layout = new StackPane();
+        var scene = new Scene(layout, 640, 480);
+
+        Button boton = new Button();
+        boton.setText("Jugar");
+        boton.setOnAction(e -> {
+            System.out.println("Comienza el juego");
+        });
+
+        layout.getChildren().add(boton);
+
+
+        stage.setTitle("GPS Challenge");
         stage.setScene(scene);
         stage.show();
     }
