72b6f462945662d7c871e17b61bba5ea7a58c7cf
alejofabregas <afabregas@fi.uba.ar>
2022-06-30 04:22:27 -0300

    Sonidos de botones

--- src/main/java/edu/fiuba/algo3/vista/Manejadores/BotonJugarEventHandler.java
+++ src/main/java/edu/fiuba/algo3/vista/Manejadores/BotonJugarEventHandler.java
@@ -3,12 +3,18 @@ package edu.fiuba.algo3.vista.Manejadores;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class BotonJugarEventHandler implements EventHandler<ActionEvent> {
 
     Stage stage;
     Scene proximaEscena;
+
+    private final static MediaPlayer sonidoClickear = new MediaPlayer(new Media(new File("src/main/java/edu/fiuba/algo3/vista/sonidoClickear.mp3").toURI().toString()));
 
     public BotonJugarEventHandler(Stage stage, Scene proximaEscena){
         this.proximaEscena = proximaEscena;
@@ -17,6 +23,8 @@ public class BotonJugarEventHandler implements EventHandler<ActionEvent> {
 
     @Override
     public void handle(ActionEvent actionEvent) {
+        sonidoClickear.play();
+        sonidoClickear.seek(sonidoClickear.getStartTime());
         this.stage.setScene(this.proximaEscena);
     }
 }
--- src/main/java/edu/fiuba/algo3/vista/Manejadores/BotonOpcionesEventHandler.java
+++ src/main/java/edu/fiuba/algo3/vista/Manejadores/BotonOpcionesEventHandler.java
@@ -3,12 +3,18 @@ package edu.fiuba.algo3.vista.Manejadores;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class BotonOpcionesEventHandler implements EventHandler<ActionEvent> {
 
     Stage stage;
     Scene proximaEscena;
+
+    private final static MediaPlayer sonidoOpciones = new MediaPlayer(new Media(new File("src/main/java/edu/fiuba/algo3/vista/sonidoSeleccionar.mp3").toURI().toString()));
 
     public BotonOpcionesEventHandler(Stage stage, Scene proximaEscena){
         this.proximaEscena = proximaEscena;
@@ -16,5 +22,9 @@ public class BotonOpcionesEventHandler implements EventHandler<ActionEvent> {
     }
 
     @Override
-    public void handle(ActionEvent actionEvent) {this.stage.setScene(this.proximaEscena);}
+    public void handle(ActionEvent actionEvent) {
+        sonidoOpciones.play();
+        sonidoOpciones.seek(sonidoOpciones.getStartTime());
+        this.stage.setScene(this.proximaEscena);
+    }
 }
