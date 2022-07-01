8ca07f7cc220e9d4d729dd2184c29c486e06aa52
NestorPala <npalavecino@fi.uba.ar>
2022-07-01 10:54:59 -0300

    cambio opacidad de los chocables

--- src/main/java/edu/fiuba/algo3/view/Imagenes.java
+++ src/main/java/edu/fiuba/algo3/view/Imagenes.java
@@ -19,37 +19,37 @@ public class Imagenes {
         if (chocable instanceof ControlPolicial) {
             ImageView imagen = new ImageView();
             imagen.setImage(new Image(new File("src/main/java/edu/fiuba/algo3/view/fotos/police.png").toURI().toString()));
-            imagen.setOpacity(0.5);
+            imagen.setOpacity(0.75);
             return imagen;
         }
         if (chocable instanceof Pozo) {
             ImageView imagen = new ImageView();
             imagen.setImage(new Image(new File("src/main/java/edu/fiuba/algo3/view/fotos/pozo.png").toURI().toString()));
-            imagen.setOpacity(0.5);
+            imagen.setOpacity(0.75);
             return imagen;
         }
         if (chocable instanceof Piquete) {
             ImageView imagen = new ImageView();
             imagen.setImage(new Image(new File("src/main/java/edu/fiuba/algo3/view/fotos/manifestation.png").toURI().toString()));
-            imagen.setOpacity(0.5);
+            imagen.setOpacity(0.75);
             return imagen;
         }
         if (chocable instanceof SorpresaFavorable) {
             ImageView imagen = new ImageView();
             imagen.setImage(new Image(new File("src/main/java/edu/fiuba/algo3/view/fotos/gift.png").toURI().toString()));
-            imagen.setOpacity(0.5);
+            imagen.setOpacity(0.75);
             return imagen;
         }
         if (chocable instanceof SorpresaDesfavorable) {
             ImageView imagen = new ImageView();
             imagen.setImage(new Image(new File("src/main/java/edu/fiuba/algo3/view/fotos/gift.png").toURI().toString()));
-            imagen.setOpacity(0.5);
+            imagen.setOpacity(0.75);
             return imagen;
         }
         if (chocable instanceof SorpresaCambioVehiculo) {
             ImageView imagen = new ImageView();
             imagen.setImage(new Image(new File("src/main/java/edu/fiuba/algo3/view/fotos/gift.png").toURI().toString()));
-            imagen.setOpacity(0.5);
+            imagen.setOpacity(0.75);
             return imagen;
         }
         if (chocable instanceof Meta) {
