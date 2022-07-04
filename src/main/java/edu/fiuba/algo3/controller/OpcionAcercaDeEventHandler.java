db777af1f2081761ae3eead4e31aaa762fd105ed
lorenzoahumada <lorenzosahumada@gmail.com>
2022-07-04 01:38:30 -0300

    Agrego alerta en barra de menu

--- src/main/java/edu/fiuba/algo3/controller/OpcionAcercaDeEventHandler.java
+++ src/main/java/edu/fiuba/algo3/controller/OpcionAcercaDeEventHandler.java
@@ -2,11 +2,16 @@ package edu.fiuba.algo3.controller;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
 
 public class OpcionAcercaDeEventHandler implements EventHandler<ActionEvent> {
 
     @Override
     public void handle(ActionEvent actionEvent) {
-        System.out.println("Trabajo practico Algoritmos III");
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Acerca de...");
+        alert.setHeaderText("Trabajo practico de algoritmos y programacion III");
+        alert.setContentText("Integrantes: \n\nMatias Vallejos\nAlejo Fabregas\nNestor Palavecino\nLorenzo Ahumada");
+        alert.show();
     }
 }
